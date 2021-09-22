d328799767b45cdd5adb3b0015574df10c68db2e
Roland Strasser <rol1510@users.noreply.github.com>
2021-09-22 17:54:01 +0200

    add firebase hosting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRIjkCsgI3iiWYJurXmDGy5bcS9Y4VBtc",
+  authDomain: "personalpage-10c9d.firebaseapp.com",
+  databaseURL:
+    "https://personalpage-10c9d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "personalpage-10c9d",
+  storageBucket: "personalpage-10c9d.appspot.com",
+  messagingSenderId: "251640634883",
+  appId: "1:251640634883:web:83768947f60cb03a363836",
+  measurementId: "G-YML59MPLP5",
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>

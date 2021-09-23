03511d938e52742b21baa1cd19de2def6300a530
Roland Strasser <rol1510@users.noreply.github.com>
2021-09-23 18:45:13 +0200

    add profile pic backgrond animation

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,42 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import profileImg from "../imgs/Foto-2020-2.png";
 import skillsImg from "../imgs/skills.png";
 import skills2Img from "../imgs/skills-2.png";
 import educationImg from "../imgs/education.png";
 
 function MeCard(props) {
+  // 😃 or 😊 (on hover)
+  const [currentEmoji, setEmoji] = useState("😃");
+
   return (
-    <div className="my-8 md:my-16 flex justify-center">
-      <div className="">
-        <div className="flex items-center">
+    <div className="relative my-8 md:my-16 flex justify-center items-center">
+      <div className="relative flex items-center justify-center w-72 h-72 ">
+        <div className="absolute top-0 left-0 w-full h-full ">
           <div
-            className="overflow-hidden rounded-full w-56 h-56 border-4
-                       border-gray-500 shadow-md"
-          >
-            <img
-              className="relative -top-2 object-cover"
-              src={profileImg}
-              alt="Roland in suite"
-            />
-          </div>
-          <div className="ml-8">
-            <p className="text-3xl font-round mb-4">Hallo! 😃</p>
-            <p>
-              <span className="text-2xl font-round mb-2 text-gray-800">
-                {/* {"Ich heiße"} */}
-              </span>
-              <span className="text-4xl font-round mb-2 text-blue-700 font-bold">
-                {" Roland Strasser"}
-              </span>
-            </p>
-            <p className="text-md font-round text-gray-500">
-              Student TU-Wien | Front-End Entwickler
-            </p>
-            <p className="text-md font-round text-gray-500">
-              Links Github LinkedIn
-            </p>
-          </div>
+            className="absolute top-0 left-0
+            bg-indigo-500 opacity-50 w-52 h-52 rounded-full
+            mix-blend-multiply filter blur-lg
+            animate-move "
+          />
+          <div
+            className="absolute top-0 left-0
+            bg-blue-300 w-52 h-52 rounded-full
+            mix-blend-multiply filter blur-lg
+            animate-move animation-delay-1"
+          />
+          <div
+            className="absolute top-0 left-0
+            bg-blue-600 opacity-50 w-52 h-52 rounded-full
+            mix-blend-multiply filter blur-lg
+            animate-move animation-delay-2"
+          />
         </div>
+
+        <div
+          className="overflow-hidden rounded-full w-56 h-56 border-4
+                       border-gray-500 shadow-md"
+        >
+          <img
+            className="relative -top-2 object-cover"
+            src={profileImg}
+            alt="Roland in suite"
+          />
+        </div>
+      </div>
+
+      <div className="ml-8">
+        <p
+          className="text-3xl font-round mb-4"
+          onMouseEnter={() => {
+            setEmoji("😊");
+          }}
+          onMouseLeave={() => {
+            setEmoji("😃");
+          }}
+        >
+          Hallo! {currentEmoji}
+        </p>
+
+        <p>
+          <span className="text-2xl font-round mb-2 text-gray-800">
+            {/* {"Ich heiße"} */}
+          </span>
+          <span className="text-4xl font-round mb-2 text-blue-700 font-bold">
+            {" Roland Strasser"}
+          </span>
+        </p>
+        <p className="text-md font-round text-gray-500">
+          Student TU-Wien | Front-End Entwickler
+        </p>
+        <p className="text-md font-round text-gray-500">
+          Links Github LinkedIn
+        </p>
       </div>
     </div>
   );
@@ -115,8 +149,8 @@ class LandingPage extends React.Component {
       <div>
         <MeCard />
         <div
-          className="angle-top pt-40 pb-40
-                     bg-gray-200 flex flex-col items-center px-10"
+          className="angle-top pt-40 pb-40 px-10
+                     bg-gray-200 flex flex-col items-center"
         >
           <Card title="About Me" img={skillsImg}>
             <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       animation: {
         wiggle: "wiggle 2s ease-in-out 1",
         appear: "appear 250ms ease-in-out 1",
+        move: "random-move 24s ease-in-out infinite",
       },
       keyframes: {
         wiggle: {
@@ -19,6 +20,20 @@ module.exports = {
         appear: {
           "0%": { "transform-origin": "top right", transform: "scale(0)" },
           "100%": { transform: "scale(1)" },
+        },
+        "random-move": {
+          "0% 100%": {
+            "transform-origin": "bottom right",
+            transform: "translate(-0.5rem, 0) scale(1)",
+          },
+          "33%": {
+            "transform-origin": "bottom right",
+            transform: "translate(5rem, 3.5rem) scale(1.1)",
+          },
+          "66%": {
+            "transform-origin": "bottom right",
+            transform: "translate(-1rem, 6rem) scale(.9)",
+          },
         },
       },
       spacing: {

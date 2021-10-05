f6b19b2a712a2c06aeecc256ca78a151fb748d06
Roland Strasser <rol1510@users.noreply.github.com>
2021-10-05 20:40:47 +0200

    refactor
    ups

--- src/index.js
+++ src/index.js
@@ -4,21 +4,9 @@ import "./index.css";
 import App from "./App";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "@firebase/analytics";
+import secrets from "./secrets";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDRIjkCsgI3iiWYJurXmDGy5bcS9Y4VBtc",
-  authDomain: "personalpage-10c9d.firebaseapp.com",
-  databaseURL:
-    "https://personalpage-10c9d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "personalpage-10c9d",
-  storageBucket: "personalpage-10c9d.appspot.com",
-  messagingSenderId: "251640634883",
-  appId: "1:251640634883:web:83768947f60cb03a363836",
-  measurementId: "G-YML59MPLP5",
-};
-
-initializeApp(firebaseConfig);
+initializeApp(secrets.FIREBASE_CONFIG);
 const analytics = getAnalytics();
 
 ReactDOM.render(

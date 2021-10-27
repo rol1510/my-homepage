42c203c07624cdf24c5f53a7f7c4102419ba7eb0
Roland Strasser <rol1510@users.noreply.github.com>
2021-10-27 21:04:22 +0200

    do not purge scale classes in production

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -68,8 +68,14 @@ class SiteViewer extends React.Component {
 }
 
 export default function ProjectsPage(props) {
+  const keepTailwindClasses =
+    "scale-2 scale-4 scale-6 scale-8 scale-10 scale-12 scale-14 scale-16 scale-18 scale-20 scale-22 scale-24 scale-26 scale-28 scale-30 scale-32 scale-34 scale-36 scale-38 scale-40 scale-42 scale-44 scale-46 scale-48 scale-50 scale-52 scale-54 scale-56 scale-58 scale-60 scale-62 scale-64 scale-66 scale-68 scale-70 scale-72 scale-74 scale-76 scale-78 scale-80 scale-82 scale-84 scale-86 scale-88 scale-90 scale-92 scale-94 scale-96 scale-98";
+
   return (
     <div className="p-10">
+      {/* hack to keep tailwind from purging those classes in prod */}
+      <div className={`absolute -left-96 ${keepTailwindClasses}`}></div>
+
       <div
         className="bg-gray-100 p-6 mx-auto mt-10 h-auto
                    px-6 mobile:mx-5 h-96
